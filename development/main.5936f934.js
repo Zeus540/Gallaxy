2ce7e36922748d84b4e16b01bae6f962c2df518c
Zaheer <zaheerroberts4@gmail.com>
2021-02-05 12:14:29 +0200

    added navigation

--- development/main.5936f934.js
+++ development/main.5936f934.js
@@ -121,23 +121,41 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 //Scrolls to the Sun//
 var Sun = document.getElementById('Sun'); //Navigation//
 
-var Up = document.getElementById('up');
+var N = document.getElementById('N');
+var NW = document.getElementById('NW');
 var Down = document.getElementById('down');
-var Down = document.getElementById('down');
+var Left = document.getElementById('left');
+var Right = document.getElementById('right');
 
 function Scroll() {
   alignToTop = true;
+  document.body.style.overflow = 'hidden';
   Sun.scrollIntoView(alignToTop);
   console.log('scrolled');
 
-  Up.onclick = function () {
+  N.onclick = function () {
     Row1m.scrollIntoView(alignToTop);
-    console.log('scrolled to Rowm1');
+    console.log('scrolled to Row1m');
+  };
+
+  NW.onclick = function () {
+    Row1l.scrollIntoView(alignToTop);
+    console.log('scrolled to Row1m');
   };
 
   Down.onclick = function () {
     Row3m.scrollIntoView(alignToTop);
-    console.log('scrolled to Rowm3');
+    console.log('scrolled to Row3m');
+  };
+
+  Left.onclick = function () {
+    Row2l.scrollIntoView(alignToTop);
+    console.log('scrolled to Row2l');
+  };
+
+  Right.onclick = function () {
+    Row2r.scrollIntoView(alignToTop);
+    console.log('scrolled to Row2r');
   };
 }
 
--- src/js/scrollto.js
+++ src/js/scrollto.js
@@ -2,26 +2,43 @@
  var Sun = document.getElementById('Sun');
 
  //Navigation//
- var Up = document.getElementById('up');
+ var N = document.getElementById('N');
+ var NW = document.getElementById('NW');
  var Down = document.getElementById('down');
- var Down = document.getElementById('down');
+ var Left = document.getElementById('left');
+ var Right = document.getElementById('right');
+
 function Scroll(){
     alignToTop = true;
-
+    document.body.style.overflow = 'hidden';
     Sun.scrollIntoView(alignToTop);
     console.log('scrolled');
     
-    Up.onclick = () =>{
+    N.onclick = () =>{
         Row1m.scrollIntoView(alignToTop);
-        console.log('scrolled to Rowm1')
+        console.log('scrolled to Row1m')
     }
 
+    NW.onclick = () =>{
+        Row1l.scrollIntoView(alignToTop);
+        console.log('scrolled to Row1m')
+    }
      
     Down.onclick = () =>{
         Row3m.scrollIntoView(alignToTop);
-        console.log('scrolled to Rowm3')
+        console.log('scrolled to Row3m')
     }
-   
+
+    Left.onclick = () =>{
+        Row2l.scrollIntoView(alignToTop);
+        console.log('scrolled to Row2l')
+    }
+
+    
+    Right.onclick = () =>{
+        Row2r.scrollIntoView(alignToTop);
+        console.log('scrolled to Row2r')
+    }
    
 }
 Scroll()
